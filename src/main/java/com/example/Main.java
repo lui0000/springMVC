8711214530c2dae4f431ba95a7dfbafe52081ea8
Lisa <elbondarenko04@gmail.com>
2024-02-26 12:11:24 +0300

    object scope prototype

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,9 +6,18 @@ public class Main {
     public static void main(String[] args) {
        
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        // MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        musicPlayer.playMusic();
+        MusicPlayer firstmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comperison = firstmusicPlayer == secondmusicPlayer;
+
+        System.out.println(firstmusicPlayer);
+        System.out.println(secondmusicPlayer);
+
+        
+        System.out.println(comperison);
 
         // System.out.println("Name: " + musicPlayer.getName());
         // System.out.println("Volume: " + musicPlayer.getVolume());
